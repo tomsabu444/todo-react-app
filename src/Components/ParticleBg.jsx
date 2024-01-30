54366a2923ba1a914b320b4302e2c25175808620
TOM SABU <tomsabu0000@gmail.com>
2024-01-30 22:39:24 +0530

    --ParticleBg gradient test

--- src/Components/ParticleBg.jsx
+++ src/Components/ParticleBg.jsx
@@ -27,6 +27,21 @@ function ParticleBg()  {
     console.log(container);
   };
 
+//   var granimInstance = new Granim({
+//     element: '#canvas-basic',
+//     direction: 'left-right',
+//     isPausedWhenNotInView: true,
+//     states : {
+//         "default-state": {
+//             gradients: [
+//                 ['#ff9966', '#ff5e62'],
+//                 ['#00F260', '#0575E6'],
+//                 ['#e1eec3', '#f05053']
+//             ]
+//         }
+//     }
+// });
+
   const options = useMemo(
     () => ({
       background: {
